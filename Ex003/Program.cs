f1664f7950007ba57f2ad9ca5d01171347ecc067
Sergei Kamianetskii <i@ksergey.ru>
2023-02-27 11:10:57 +0300

    Добавлена фича проверки факта делимости числа

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -5,12 +5,11 @@ int GetNumber(string text)
   return Convert.ToInt32(Console.ReadLine());
 }
 
+bool Check(int value)
+{
+  return value % 7 == 0 && value % 23 == 0;
+}
+
 int value = GetNumber("Введите value: ");
-if (value % 7 == 0 && value % 23 == 0)
-{
-  Console.WriteLine("YES");
-}
-else
-{
-  Console.WriteLine("NO");
-}
+bool flag = Check(value);
+System.Console.WriteLine(flag);
