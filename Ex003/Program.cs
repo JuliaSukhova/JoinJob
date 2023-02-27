6d9a2b9c6edeeaa14f2de413f6f11b157b0224bb
Sergei Kamianetskii <i@ksergey.ru>
2023-02-27 11:01:09 +0300

    Добавленл решение задачи №3

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите value: ");
+
+// int value = 14;
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value % 7 == 0 && value % 23 == 0)
+{
+  Console.WriteLine("YES");
+}
+else
+{
+  Console.WriteLine("NO");
+}
