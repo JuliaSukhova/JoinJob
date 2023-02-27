225da7acad87ae588ebebc65e807da8f419a5102
Sergei Kamianetskii <i@ksergey.ru>
2023-02-27 11:16:14 +0300

    Добавлен метод вывода результат. Задача решена

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -10,6 +10,15 @@ bool Check(int value)
   return value % 7 == 0 && value % 23 == 0;
 }
 
+string GetResult(int number, bool checkResult)
+{
+  string temp = String.Empty;
+  if (checkResult) { temp = "YES"; }
+  else { temp = "NO"; }
+  return "Число: " + number + ". Итог: " + temp;
+}
+
 int value = GetNumber("Введите value: ");
 bool flag = Check(value);
-System.Console.WriteLine(flag);
+string output = GetResult(value, flag);
+System.Console.WriteLine(output);
